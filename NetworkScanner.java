b38f597b99e67429dfb19b3b7e37a07039ee470e
Nathan_Nakawatase <92493058+NathanNakawatase@users.noreply.github.com>
2022-07-25 20:34:59 -0700

    Add files via upload

--- NetworkScanner.java
+++ NetworkScanner.java
@@ -1,5 +1,6 @@
 import java.net.Socket;
-
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 public class NetworkScanner
 {
   
@@ -11,21 +12,63 @@ public class NetworkScanner
    System.out.println("Attempting to scan " + hostname + ":" + port);
    try {
    Socket mysocket = new Socket(hostname, port);
+   mysocket.setSoTimeout(20);
    return true;
-   } //returns false if an exception is thrown
+   } 
    catch(Exception catchall) {
    return false;
    }
  }
+public boolean scanAll(String hostname) {
+
+  boolean[] results = new boolean[65535]; 
+
+  try{
+    FileWriter file = new FileWriter(hostname);
+    BufferedWriter out = new BufferedWriter(file);
+
+    // For debugging, only scanning ports 0-3
+    
+ for (int y = 0 ; y < 3 ; y++) {
+    results[y] = scan(hostname, y);
+    if (results[y] == true) {
+      
+        out.write(y +":OPEN\n");
+        System.out.println("Port OPEN :" + y);
+    }
+    else {
+        out.write(y+":CLOSED\n");
+        System.out.println("Port CLOSED:" + y);
+    }
+ }
+ out.close();
+  }
+  catch(Exception e) {
+  }
+  
+   return true;
+}
+
  
+
+
+
+
+
  public static void main (String [] args ) {
  
     NetworkScanner networkScanner = new NetworkScanner();
-     
+
+      
     for (int x = 0 ; x < args.length ; x++) 
+ {
+
     {
-     System.out.println("Scanning:" + args[x]); 
-     System.out.println(networkScanner.scan(args[x],443));
+      System.out.println("encountered error while creating file");
+    }
+     System.out.println("Scanning:" + args[x]);
+     System.out.println(networkScanner.scanAll(args[x]));
+     
     }
     
 
